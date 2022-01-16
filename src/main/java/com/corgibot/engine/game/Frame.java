9a6055251502675e2c7d161d976d98ff278ad49b
Daniel Schulz <daniel.schulz@workmotion.com>
2022-01-16 14:44:25 +0100

    Rollback buffering

--- src/main/java/com/corgibot/engine/game/Frame.java
+++ src/main/java/com/corgibot/engine/game/Frame.java
@@ -112,21 +112,16 @@ public class Frame {
     private class Canvas extends JPanel {
         @Override
         protected void paintComponent(Graphics g) {
-            Image next = createImage(size * blockSize, size * blockSize);
-            Graphics nextGraphics = next.getGraphics();
-
             // TODO buffer next frame
-            nextGraphics.setColor(HEADER_COLOR);
-            nextGraphics.fillRect(0, 0, blockSize * size, MARGIN_TOP);
-            nextGraphics.setColor(Color.black);
-            nextGraphics.drawString(text, 0, (int) (MARGIN_TOP / 1.5));
+            g.setColor(HEADER_COLOR);
+            g.fillRect(0, 0, blockSize * size, MARGIN_TOP);
+            g.setColor(Color.black);
+            g.drawString(text, 0, (int) (MARGIN_TOP / 1.5));
 
             while (actions.size() > 0) {
                 Consumer<Graphics> action = actions.remove();
-                action.accept(nextGraphics);
+                action.accept(g);
             }
-
-            g.drawImage(next, 0, 0, null);
         }
     }
 }

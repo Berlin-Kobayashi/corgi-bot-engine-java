4ff7de099fcd1f87f6a1188fbd1bddd560dfd43a
Daniel Schulz <daniel.schulz@workmotion.com>
2022-01-16 09:34:10 +0100

    Fix frame rate

--- src/main/java/com/corgibot/engine/game/Game.java
+++ src/main/java/com/corgibot/engine/game/Game.java
@@ -2,11 +2,16 @@ package com.corgibot.engine.game;
 
 import com.corgibot.utils.time.Time;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
 import java.util.function.Consumer;
 
 public class Game {
     public static GameConfig config;
     private boolean isRunning;
+    private Instant lastDraw;
     private final Frame frame;
     private Consumer<Frame> frameHandler;
 
@@ -25,13 +30,21 @@ public class Game {
 
     public void start() {
         isRunning = true;
-
+        Duration frameDuration = Duration.of(config.getFrameDuration(), ChronoUnit.MILLIS);
         while (isRunning) {
             if (frameHandler != null) {
-                frameHandler.accept(frame);
-                frame.draw();
+                Instant now = Instant.now();
+                Duration sinceLastDraw = lastDraw == null ? frameDuration : Duration.between(lastDraw, now);
+                long delay = sinceLastDraw.minus(frameDuration).toMillis();
+                if (delay >= 0) {
+                    frameHandler.accept(frame);
+                    frame.draw();
+                    lastDraw = Instant.now();
 
-                Time.sleep(config.getFrameDuration());
+                    if (delay > 0) {
+                        System.out.println("Frame delay of: " + delay + " ms");
+                    }
+                }
             }
         }
     }

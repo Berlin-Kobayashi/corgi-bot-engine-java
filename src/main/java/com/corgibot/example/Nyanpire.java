28318778f67095711f6163d2750527d5e59dba88
Daniel Schulz <daniel.schulz@workmotion.com>
2022-01-22 01:26:01 +0100

    Add aimed projectile

--- src/main/java/com/corgibot/example/Nyanpire.java
+++ src/main/java/com/corgibot/example/Nyanpire.java
@@ -11,14 +11,18 @@ import com.corgibot.utils.random.Random;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class Nyanpire {
+    private static final int MAX_YOKAI = 3;
     private static final Game game = new Game(new GameConfig(50, 13, Color.darkGray));
 
     private static final Player player = new Player(getMidPosition(), "Nyanpire/Player");
     private static final List<Yokai> yokais = new ArrayList<>();
+    private static final Map<Yokai, AimedProjectile> aimedProjectiles = new HashMap<>();
 
     static class Player {
         private final Position position;
@@ -60,44 +64,121 @@ public class Nyanpire {
         }
     }
 
+    static class AimedProjectile {
+        private final Position position;
+        private final Position nextPosition;
+        private final String image;
+        private final Yokai target;
+
+        private AimedProjectile(Position position, String image, Yokai target) {
+            this.position = new Position(position.x, position.y);
+            this.nextPosition = new Position(position.x, position.y);
+            this.image = image;
+            this.target = target;
+        }
+
+        private void moveTowardsTarget() {
+            if (target.position.x < position.x) {
+                nextPosition.x--;
+            } else {
+                if (target.position.x > position.x) {
+                    nextPosition.x++;
+                }
+            }
+
+            if (target.position.y < position.y) {
+                nextPosition.y--;
+            } else {
+                if (target.position.y > position.y) {
+                    nextPosition.y++;
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Speaker.loop("Nyanpire/Music");
-        Keyboard.onKey(KeyEvent.VK_LEFT, Nyanpire::moveAllYokaiRight);
-        Keyboard.onKey(KeyEvent.VK_RIGHT, Nyanpire::moveAllYokaiLeft);
-        Keyboard.onKey(KeyEvent.VK_UP, Nyanpire::moveAllYokaiDown);
-        Keyboard.onKey(KeyEvent.VK_DOWN, Nyanpire::moveAllYokaiUp);
+        Keyboard.onKey(KeyEvent.VK_LEFT, Nyanpire::moveWorldRight);
+        Keyboard.onKey(KeyEvent.VK_RIGHT, Nyanpire::moveWorldLeft);
+        Keyboard.onKey(KeyEvent.VK_UP, Nyanpire::moveWorldDown);
+        Keyboard.onKey(KeyEvent.VK_DOWN, Nyanpire::moveWorldUp);
 
         game.onFrame(Nyanpire::onFrame);
 
         game.start();
     }
 
-    private static void moveAllYokaiLeft() {
+    private static void moveWorldLeft() {
+        aimedProjectiles.values().forEach(aimedProjectile -> aimedProjectile.nextPosition.x--);
         yokais.forEach(yokai -> yokai.nextPosition.x--);
     }
 
-    private static void moveAllYokaiRight() {
+    private static void moveWorldRight() {
+
+        aimedProjectiles.values().forEach(aimedProjectile -> aimedProjectile.nextPosition.x++);
+
         yokais.forEach(yokai -> yokai.nextPosition.x++);
     }
 
-    private static void moveAllYokaiUp() {
+    private static void moveWorldUp() {
+        aimedProjectiles.values().forEach(aimedProjectile -> aimedProjectile.nextPosition.y--);
         yokais.forEach(yokai -> yokai.nextPosition.y--);
     }
 
-    private static void moveAllYokaiDown() {
+    private static void moveWorldDown() {
+        aimedProjectiles.values().forEach(aimedProjectile -> aimedProjectile.nextPosition.y++);
         yokais.forEach(yokai -> yokai.nextPosition.y++);
     }
 
     private static void onFrame(Frame frame) {
         frame.drawImage(player.position, player.image);
         updateCharacterPosition(frame);
-        if (frame.getCounter() % 60 == 0) {
-            moveYokais();
+        updateProjectiles(frame);
+        if (frame.getCounter() != 0) {
+            if (frame.getCounter() % 60 == 0) {
+                moveYokais();
+            }
+            if (frame.getCounter() % 30 == 0) {
+                fireWeapons();
+                moveProjectiles();
+            }
+            if (frame.getCounter() % (180 / ((frame.getCounter() / 180) + 1)) == 0) {
+                spawnYokai();
+            }
         }
+    }
 
-        if (frame.getCounter() % 180 == 0) {
-            spawnYokai();
-        }
+    private static void updateProjectiles(Frame frame) {
+        List<Yokai> finishedProjectiles = new ArrayList<>();
+        aimedProjectiles.values().forEach(aimedProjectile -> {
+                    frame.erase(aimedProjectile.position);
+                    if (aimedProjectile.nextPosition.equals(aimedProjectile.target.position)) {
+                        Speaker.play("Nyanpire/Death");
+                        frame.erase(aimedProjectile.nextPosition);
+                        yokais.remove(aimedProjectile.target);
+                        finishedProjectiles.add(aimedProjectile.target);
+                    } else {
+                        frame.drawImage(aimedProjectile.nextPosition, aimedProjectile.image);
+                        aimedProjectile.position.x = aimedProjectile.nextPosition.x;
+                        aimedProjectile.position.y = aimedProjectile.nextPosition.y;
+                    }
+                }
+        );
+
+        finishedProjectiles.forEach(aimedProjectiles::remove);
+    }
+
+    private static void moveProjectiles() {
+        aimedProjectiles.values().forEach(AimedProjectile::moveTowardsTarget);
+    }
+
+    private static void fireWeapons() {
+        Speaker.play("Nyanpire/Flame");
+        yokais.forEach(yokai -> {
+            if (!aimedProjectiles.containsKey(yokai)) {
+                aimedProjectiles.put(yokai, new AimedProjectile(player.position, "Nyanpire/Flame", yokai));
+            }
+        });
     }
 
     private static void moveYokais() {
@@ -105,7 +186,9 @@ public class Nyanpire {
     }
 
     private static void spawnYokai() {
-        yokais.add(new Yokai(randomOuterPosition(), "Nyanpire/Yokai"));
+        if (yokais.size() < MAX_YOKAI) {
+            yokais.add(new Yokai(randomOuterPosition(), "Nyanpire/Yokai"));
+        }
     }
 
     private static Position randomOuterPosition() {

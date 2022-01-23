894b6a4936403dc4298074990a9053264f239843
Daniel Schulz <daniel.schulz@workmotion.com>
2022-01-23 17:48:22 +0100

    Support parallel plays of same sounds

--- src/main/java/com/corgibot/engine/audio/Speaker.java
+++ src/main/java/com/corgibot/engine/audio/Speaker.java
@@ -1,16 +1,37 @@
 package com.corgibot.engine.audio;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
 import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 
 // TODO use event queue speaker actions
 public class Speaker {
-    private static final Map<String, Clip> sounds = new HashMap<>();
+    private static final Map<String, Sound> sounds = new HashMap<>();
+
+    private static class Sound {
+        private final byte[] data;
+        private final AudioFormat format;
+
+        private Sound(byte[] data, AudioFormat format) {
+            this.data = data;
+            this.format = format;
+        }
+
+        private Clip getClip() throws LineUnavailableException {
+            Clip clip = AudioSystem.getClip();
+            clip.open(this.format, this.data, 0, this.data.length);
+
+            return clip;
+        }
+    }
 
     public static void play(String soundName) {
         play(soundName, 0);
@@ -19,13 +40,11 @@ public class Speaker {
     public static void play(String soundName, int volume) {
         try {
             Clip clip = getSound(soundName);
-            if (!clip.isRunning()) {
-                clip.setFramePosition(0);
-                FloatControl gainControl =
-                        (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
-                gainControl.setValue(volume);
-                clip.start();
-            }
+            clip.setFramePosition(0);
+            FloatControl gainControl =
+                    (FloatControl) clip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(volume);
+            clip.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -38,6 +57,7 @@ public class Speaker {
     public static void loop(String soundName, int volume) {
         try {
             Clip clip = getSound(soundName);
+
             if (!clip.isRunning()) {
                 clip.setFramePosition(0);
                 FloatControl gainControl =
@@ -50,24 +70,18 @@ public class Speaker {
         }
     }
 
-    private static Clip getSound(String soundName) {
-        loadSoundOnce(soundName);
-        return sounds.get(soundName);
+    private static Clip getSound(String soundName) throws UnsupportedAudioFileException, IOException, LineUnavailableException {
+        Sound sound = loadSoundOnce(soundName);
+        return sound.getClip();
     }
 
-    private static void loadSoundOnce(String soundName) {
-        try {
-            if (!sounds.containsKey(soundName)) {
-                String path = System.getProperty("user.dir") + "/assets/sounds/" + soundName + ".wav";
-                AudioInputStream audioIn = AudioSystem.getAudioInputStream(new URL("file://" + path));
-
-                Clip clip = AudioSystem.getClip();
-                clip.open(audioIn);
-
-                sounds.put(soundName, clip);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+    private static Sound loadSoundOnce(String soundName) throws IOException, UnsupportedAudioFileException {
+        if (!sounds.containsKey(soundName)) {
+            String path = System.getProperty("user.dir") + "/assets/sounds/" + soundName + ".wav";
+            AudioInputStream audioIn = AudioSystem.getAudioInputStream(new URL("file://" + path));
+            sounds.put(soundName, new Sound(audioIn.readAllBytes(), audioIn.getFormat()));
         }
+
+        return sounds.get(soundName);
     }
 }
--- src/main/java/com/corgibot/example/Demo.java
+++ src/main/java/com/corgibot/example/Demo.java
@@ -1,6 +1,8 @@
 package com.corgibot.example;
 
+import com.corgibot.engine.audio.Speaker;
 import com.corgibot.engine.control.Mouse;
+import com.corgibot.engine.game.GameConfig;
 import com.corgibot.engine.game.Raster;
 import com.corgibot.engine.game.Game;
 
@@ -8,12 +10,12 @@ import java.awt.*;
 
 public class Demo {
     public static void main(String[] args) {
-        Game game = new Game();
+        Game game = new Game(new GameConfig(16, 1000, Color.black));
         game.onFrame(Demo::onFrame);
         game.start();
     }
 
     private static void onFrame(Game game) {
-        game.raster.drawBlock(Mouse.getPosition(), Color.BLACK);
+        Speaker.play("Knyacki/Aitz");
     }
 }
